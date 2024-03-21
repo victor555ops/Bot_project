c0faa2842527f09287bcfb3bd8c675b0de499628
David Sobrevilla <150841434+018x@users.noreply.github.com>
2024-03-20 23:43:06 -0600

    Add files via upload

---
+++ Bot_project/BotAssist.py
@@ -0,0 +1,348 @@
+import speech_recognition as sr 
+import webbrowser
+import tkinter as tk
+import pyttsx3
+import datetime 
+import requests 
+import sys 
+import pywhatkit
+import os
+from PIL import ImageTk, Image, ImageFilter, ImageOps
+import time
+
+r = sr.Recognizer()
+engine = pyttsx3.init()
+
+engine.setProperty("language", "es-ES")
+engine.setProperty("rate", 145)
+
+
+def obtener_hora():
+  hora = datetime.datetime.now().time()
+
+  hora_str = hora.strftime("%I:%M %p")
+  return hora_str
+
+print(obtener_hora())
+
+
+def abrir_segunda():
+  ventana.withdraw()
+  
+  ventana_segunda = tk.Toplevel()
+  ventana_segunda.title("Segunda ventana")
+  ventana_segunda.geometry("550x300")
+  
+  imagen2 = Image.open("paisaje2.jpg")
+  foto2 = ImageTk.PhotoImage(imagen2)
+  
+  marco = tk.Frame(ventana_segunda)
+  marco.pack()
+  
+  etiqueta = tk.Label(marco, image=foto2)
+  etiqueta.place(x=0, y=0, relwidth=1, relheight=1)
+  
+  etiqueta_2 = tk.Label(ventana_segunda, text="Bienvenido a la seccion de ayuda.\nEste programa puede hacer lo siguiente con solo picar el boton de hablar:\n- Abrir la paqueteria de office. Por ejemplo: Word.\n- Te dira la hora, fecha y estado del clima.\n- Reproduce videos en youtube.\n- Abrir google o hacer la busqueda por ti, solo tienes que pedirle tu busqueda.\nhacer preguntas interactivas.\n-por ultimo, puedes cerrar el programa diciendo cerrar programa.\n si quieres volver a la primera ventana, preciona el boton de abajo")
+  etiqueta_2.pack()
+
+  boton_2 = tk.Button(ventana_segunda, text="Volver a la primera ventana", command=lambda: volver_primera(ventana, ventana_segunda))
+  boton_2.pack()
+
+def volver_primera(ventana, ventana_segunda): 
+  ventana.deiconify()
+  ventana_segunda.destroy()
+
+
+def obtener_fecha():
+  fecha = datetime.datetime.now().date()
+  fecha_str = fecha.strftime("%d/%m/%Y")
+  return fecha_str
+
+
+def abrir_explorador():
+  os.startfile("explorer.exe")
+
+  
+def crear_carpeta(nombre):
+  escritorio = os.path.join(os.path.join(os.environ['USERPROFILE']), 'desktop')
+  carpeta = os.path.join(escritorio, nombre)
+   
+  os.makedirs(carpeta, exist_ok=True)
+  
+  
+def abrir_carpeta(nombre):
+  carpeta = os.path.join(os.path.join(os.environ['USERPROFILE']), 'Desktop', nombre)
+  
+  if os.path.exists(carpeta):
+    os.startfile(carpeta)
+  else:
+    print(f"La carpeta {nombre} no existe en el escritorio.")
+    engine.say(f"La carpeta {nombre} no existe en el escritorio")
+    engine.runAndWait()
+
+
+
+def saludar_usuario():
+  hora = datetime.datetime.now().time()
+
+  if hora.hour < 12:
+    saludo = "Hasta Pronto, que tenga un excelente dia, si necesita ayuda, no dude en contactarme"
+  elif hora.hour < 18:
+    saludo = "Hasta pronto, que tenga una excelente tarde, si necesita ayuda, no dude en contactarme"
+  else:
+    saludo = "Hasta pronto, que tenga una linda noche, si necesita ayuda no dude en contactarme"
+  
+  engine.say(saludo)
+  engine.runAndWait()
+ 
+  print(saludo)
+
+
+def escuchar_y_procesar():
+  activado = False
+  palabra_clave = "estas ahi"
+  
+  with sr.Microphone() as source:
+  
+    r.adjust_for_ambient_noise(source)
+ 
+    print("Escuchando...")
+    audio = r.listen(source,) 
+ 
+    try:
+    
+      texto = r.recognize_google(audio, language="es-ES")
+
+      print("Has dicho: " + texto)
+      
+      if activado:
+        if texto.lower() == palabra_clave.lower():
+          engine.say("si, aqui estoy.")
+          engine.runAndWait()
+          activado = False
+          time.sleep(3)
+     
+      elif "youtube" in texto.lower():
+      
+        musica = texto.lower().split("youtube")[1].strip()
+        
+        engine.say("Reproduciendo " + musica)
+        engine.runAndWait()
+      
+        print("Reproduciendo " + musica, "en youtube")
+        pywhatkit.playonyt(musica)
+   
+      elif "abrir word" in texto.lower() or "Cerrar word" in texto.lower():
+      
+        webbrowser.open("C:/Program Files/Microsoft Office/root/Office16/WINWORD.EXE")
+
+        engine.say("Abriendo Word")
+        engine.runAndWait()
+      
+        print("Abriendo Word")
+        engine.runAndWait()
+      
+      
+      elif "excel" in texto.lower():
+       
+        webbrowser.open("C:/Program Files/Microsoft Office/root/Office16/EXCEL.EXE")
+       
+        engine.say("Abriendo Excel")
+        engine.runAndWait()
+      
+        print("Abriendo Excel")
+      
+      elif "powerpoint" in texto.lower():
+       
+        webbrowser.open("C:/Program Files/Microsoft Office/root/Office16/POWERPNT.EXE")
+        
+        engine.say("Abriendo PowerPoint")
+        engine.runAndWait()
+   
+        print("Abriendo PowerPoint")
+      
+      elif "google" in texto.lower():
+     
+        if len(texto.lower().split("google")) > 1:
+         
+          busqueda = texto.lower().split("google")[1].strip()
+      
+          url = "https://www.google.com/search?q=" + busqueda
+        else:
+         
+          url = "https://www.google.com"
+       
+        webbrowser.open(url)
+        
+        engine.say("abriendo Google con la busqueda " + busqueda)
+        engine.runAndWait()
+      
+        print("Abriendo Google con la búsqueda: " + busqueda)
+      
+      elif "facebook" in texto.lower():
+        
+        webbrowser.open("https://www.facebook.com")
+        engine.say("Abriendo facebook")
+        engine.runAndWait()
+        
+      elif "whatsapp" in texto.lower():
+        
+        webbrowser.open("https://web.whatsapp.com")
+        engine.say("Abriendo whatsApp")
+        engine.runAndWait()
+        
+      elif "instagram" in texto.lower():
+        
+        webbrowser.open("https://www.instagram.com")
+        engine.say("Abriendo Instagram")
+        engine.runAndWait()
+      
+      elif "twitter" in texto.lower():
+       
+        webbrowser.open("https://twitter.com")
+        engine.say("Abriendo twitter")
+        engine.runAndWait()
+        
+      elif "tiktok" in texto.lower():
+        
+        webbrowser.open("https://www.tiktok.com/login?lang=es&redirect_url=https%3A%2F%2Fwww.tiktok.com%2Fupload%3Flang%3Des")
+        engine.say("Abriendo tiktok")
+        engine.runAndWait()
+          
+      elif "hora" in texto.lower():
+   
+        hora = obtener_hora()
+        
+        engine.say("La hora actual es " + hora)
+        engine.runAndWait()
+       
+        print("La hora actual es " + hora)
+      
+      elif "fecha" in texto.lower():
+   
+        fecha = obtener_fecha()
+    
+        engine.say("La fecha actual es " + fecha)
+        engine.runAndWait()
+       
+        print("La fecha actual es " + fecha)
+      
+      elif "cerrar programa" in texto.lower():
+     
+        saludar_usuario()
+      
+        sys.exit()
+        
+      elif "edad" in texto.lower() or "cuántos años tienes" in texto.lower():
+      
+        engine.say("Soy un modelo de asistente o bot creado desde visual studio, por lo tanto no tengo una edad definida, simplemente la fecha de mi creacion")
+        engine.runAndWait()
+      
+        print(f"Soy un modelo de asistente o bot creado desde visual studio, por lo tanto no tengo una edad defininida, simplemente la fecha de mi creacion")
+      
+      elif "gracias" in texto.lower():
+        
+        engine.say("Claro, es un placer atenderte")
+        engine.runAndWait()
+      
+      elif "clima" in texto.lower() or "tiempo" in texto.lower():
+
+      
+        api_key = "ab613795bd25da5f26bb66bfcc72d475"
+
+        lugar = "castillo de teayo"
+
+        url = f"http://api.openweathermap.org/data/2.5/weather?q={lugar}&appid={api_key}&units=metric&lang=es"
+
+        respuesta = requests.get(url)
+
+        datos = respuesta.json()
+
+        print(f"El clima en {lugar} es {datos['weather'][0]['description']}")
+        print(f"La temperatura actual es {datos['main']['temp']} °C")
+        print(f"La humedad relativa es {datos['main']['humidity']} %")
+        print(f"La presión atmosférica es {datos['main']['pressure']} hPa")
+
+        engine.say(f"El clima en {lugar} es {datos['weather'][0]['description']}")
+        engine.say(f"La temperatura actual es {datos['main']['temp']} grados centígrados")
+        engine.say(f"La humedad relativa es {datos['main']['humidity']} por ciento")
+        engine.say(f"La presión atmosférica es {datos['main']['pressure']} hectopascales")
+        engine.runAndWait()
+      
+      elif "abrir explorador" in texto.lower() or "abre explorador" in texto.lower():
+        abrir_explorador()
+        engine.say("Abriendo tu explorador de archivos")
+        engine.runAndWait()
+      
+      elif "crear carpeta" in texto.lower() or "crea carpeta" in texto.lower():
+        nombre = texto.split()[-1]
+        crear_carpeta(nombre)
+        engine.say(f"Creando la carpeta {nombre} en el escritorio")
+        engine.runAndWait()
+        
+      elif "Abrir carpeta" in texto.lower():
+        nombre = texto.split()[-1]
+        abrir_carpeta(nombre)
+        engine.say(f"Abriendo la carpeta {nombre} en el explorador de archivos")
+        engine.runAndWait()
+              
+      else:
+        
+        if texto.lower() == palabra_clave.lower():
+          engine.say("Aquí estoy, ¿en qué puedo ayudarte?")
+          engine.runAndWait()
+          activado = True
+        else:
+          engine.say("no he entendido lo que has dicho ")
+          engine.runAndWait()
+          print("No he entendido lo que has dicho")
+ 
+    except sr.UnknownValueError:
+   
+      print("No se ha podido entender el audio")
+      engine.say("No se ha podido entender el audio")
+      engine.runAndWait()
+
+
+ventana = tk.Tk()
+ventana.title("Bot Project")
+ventana.geometry("300x250")
+
+
+imagen = Image.open("paisaje.jpg")
+foto = ImageTk.PhotoImage(imagen)
+
+marco = tk.Frame(ventana)
+marco.pack()
+
+etiqueta = tk.Label(marco, image=foto)
+etiqueta.pack()
+
+tamaño_icono = (45, 45) 
+icono_hablar_img = Image.open("Mic.png")
+icono_hablar_img = icono_hablar_img.convert("RGB")
+icono_hablar_img = icono_hablar_img.filter(ImageFilter.BLUR)
+icono_hablar_img = icono_hablar_img.resize((tamaño_icono[0], tamaño_icono[1]), Image.NEAREST)
+icono_hablar = ImageTk.PhotoImage(icono_hablar_img)
+
+icono_ayuda_img = Image.open("interrogacion.jpg")
+icono_ayuda_img = icono_ayuda_img.convert("RGB")
+icono_ayuda_img = icono_ayuda_img.filter(ImageFilter.BLUR)
+icono_ayuda_img = icono_ayuda_img.resize(tamaño_icono, Image.BICUBIC)
+icono_ayuda = ImageTk.PhotoImage(icono_ayuda_img)
+
+boton = tk.Button(marco, image=icono_hablar, command=escuchar_y_procesar)
+boton_1 = tk.Button(marco, image=icono_ayuda, command=abrir_segunda)
+
+boton.place(x=10, y=60)
+boton_1.place(x=230, y=180)
+
+etiqueta_hablar = tk.Label(marco, text="Presiona para hablar")
+#etiqueta_ayuda = tk.Label(marco, text="Ayuda")
+
+etiqueta_hablar.place(x=10, y=37)
+#etiqueta_ayuda.place(x=200, y=157)
+
+ventana.mainloop()
+
+saludar_usuario()
---
+++ Bot_project/README.md
@@ -0,0 +1,140 @@
+# BotAssist 😶‍🌫️
+Nuestro repositorio esta constituido en un asistente como tal para ayudar a todas esas personas que no pueden usar la computadora, a lo largo de los años nosotros como esta edad podemos usar una computadora facil, ya sea por tutoriales, cursos, consejos de amigos etc, uno como programador puede hacer muchas cosas interesantes y eso es lo que hicimos nuestro equipo y yo para eso le damos una introduccion a nuestro codigo.
+
+Este proyecto se llama como tal :
+> BotAssist 🍕
+
+## Introduccion de Nuestro codigo 🐢
+**En este apartado es una pequeña informacion de lo que consiste nuestro codigo, en este caso puede hacer multitareas como tal, Por ejemplo:**
+
+- Nosotros podemos decirle que pueda buscar algo en google una cosa en especifica y lo hace.
+- Tambien puede abrir redes sociales ya sea, instagram, facebook, twitter, tiktok, entre otras y todo desde la busqueda de google.
+- Al igual que tambien puede darte la zona horario de Veracruz, este apartado aun no esta a modo global pero en un futuro podemos implementar eso.
+- Otro detalle como tal de busqueda de youtube es que podemos buscar cualquier cosa que sea, podemos decirle: "buscame la cancion de tal artista" y lo busca y lo 
+  reproduce en youtube
+- Para terminar tambien puede lo que es abrir las aplicaciones de Office como, Word, Excel, PowerPoint
+
+
+### CMD Y librerias 👻
+
+Para entender un poco de este apartado para ejecutar este codigo necesitamos algunas librerias instaladas en nuestro cmd, para esto podemos hacer algo muy sencillo para esto podemos usar este comando de windows para entrar al cmd.
+ `windows + r`
+ Esta linea nos llevara a una aplicacion que se llama **Ejecutar** Y es una aplicacion de Windows en el cual hay aplicacion que podemos usar.
+
+***Despues de esto es importante colocar como tal cmd en la aplicacion de ejecutar y le damos enter***.
+
+
+
+![Captura de pantalla 2024-03-14 114923](https://github.com/victor555ops/Bot_project/assets/150841434/7fff6c60-b4d9-4ce7-86cb-e751eee63221)
+
+
+
+**De esta forma asi deberia de estar**
+
+
+Y ya despues nos redesplegara la ventana de cmd en el cual instalaremos las liberias importantes para poder ejecutar este codigo que se llama **BotAssist** que se muestra a continuacion:
+
+![Captura de pantalla 2024-03-14 115427](https://github.com/victor555ops/Bot_project/assets/150841434/c483c8e7-7944-4f30-83d2-af5f6034b893)
+
+
+
+***Es importante que no a todos les saldran lo mismo como lo que son en torno a MAC***
+
+
+
+
+# Instalacion de las librerias 👽
+
+ ***Tenemos algunas liberias importadas en nuestro codigo y se requiere estas librerias para que no tengamos ningun error a la hora de ejecutar***
+
+
+Como primera libreria importada en nuestro codigo es SpeechRecognition.
+Esta librería se utiliza para reconocimiento de voz. Puedes instalarla utilizando pip:
+
+`pip install SpeechRecognition`
+
+
+## Webbrowser libreria ✔
+
+ ***Esta librería viene incluida con la instalación estándar de Python, por lo que no necesitas instalarla por separado.***
+
+
+### Tkinter ✨
+
+Para entender un poco de contexto **Tkinter** es una liberia de interfaz grafica es la interfaz por defecto de Python para el kit de herramientas de GUI Tk. Tanto Tk como tkinter están disponibles en la mayoría de las plataformas Unix, así como en sistemas Windows (Tk en sí no es parte de Python, es mantenido por ActiveState).
+
+***Es importante tener esto en cuenta para la instalacion***
+
+Ejecutar python `-m tkinter` desde la línea de comandos debería abrir una ventana que demuestre una interfaz Tk simple para saber si tkinter está instalado correctamente en su sistema. También muestra qué versión de Tcl/Tk está instalada para que pueda leer la documentación de Tcl/Tk específica de esa versión.
+
+Esta librería se utiliza para crear interfaces gráficas en Python. Si estás utilizando Python 3.x, probablemente ya la tengas instalada. Si no, puedes instalarla mediante:
+
+
+`sudo apt-get install python3-tk`  # Si estás en Linux`
+
+
+
+# Pyttsx3 🤖
+
+Es una librería de texto a voz. Puedes instalarla mediante pip:
+
+`pip install pyttsx3`  
+
+
+pyttsx3 es una biblioteca de Python que nos permite convertir texto a voz. Así que le proporcionaremos nuestro texto y convertirá ese texto en audio. Es un envoltorio alrededor de varios motores de texto a voz, incluido el motor de texto a voz (TTS) de Microsoft, en este caso nosotros usamos lo que es el Google.
+
+
+## requests 🎈
+
+Esta librería se utiliza para realizar solicitudes HTTP en Python. También se instala con pip:
+
+`pip install requests`
+
+
+La librería request es una librería de **Python** que permite hacer peticiones **HTTP**. Esta librería la podemos encontrar en `pip` donde encontraremos las versiones disponibles, las generalidades de su funcionamiento, la documentación y su repositorio.
+
+
+### Pywhatkit 🎡
+
+Esta librería se utiliza para realizar diversas acciones en WhatsApp a través de Python. Se instala con pip:
+
+`pip install pywhatkit`
+
+Es una librería de Python que te permite realizar varias acciones relacionadas con WhatsApp utilizando Python. Algunas de las características principales de pywhatkit incluyen:
+
+- Envío de mensajes de texto: Puedes enviar mensajes de texto a contactos de WhatsApp específicos desde Python.
+
+- Envío de mensajes de WhatsApp a números no guardados: La librería permite enviar mensajes de WhatsApp a números que no están guardados en tu lista de contactos.
+
+- Reproducción de contenido multimedia: pywhatkit te permite reproducir videos de YouTube, buscar y reproducir videos de YouTube utilizando el reproductor web predeterminado.
+
+- Búsqueda y reproducción de música: También puedes buscar y reproducir música de YouTube utilizando pywhatkit.
+
+- Búsqueda en Google: La librería te permite buscar en Google utilizando el navegador web predeterminado de tu sistema.
+
+- Traducción de texto: Puedes traducir texto de un idioma a otro utilizando la función de traducción de pywhatkit.
+
+
+
+# PIL 💢
+
+Esta librería se utiliza para trabajar con imágenes en Python. Puedes instalarla mediante pip:
+
+`pip install Pillow`
+
+**Pillow** es una biblioteca de procesamiento de imágenes en Python que proporciona una amplia gama de funcionalidades para abrir, manipular y guardar imágenes en varios formatos. Es una herramienta poderosa y versátil que te permite realizar tareas como: Cargar imágenes desde archivos.
+
+
+## Time 💨
+
+Y por ultimo como ultima libreria tenemos a **Time**, Esta es una librería estándar de ***Python***, por lo que no necesitas instalarla por separado.
+
+Para instalar todas las librerías a la vez, puedes ejecutar:
+
+`pip install SpeechRecognition pyttsx3 requests pywhatkit Pillow`
+
+De esta forma se instalara todas las librerias sin ningun problema..
+
+
+
+ 
